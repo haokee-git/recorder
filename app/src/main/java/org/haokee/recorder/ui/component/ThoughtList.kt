275682c1005e1af15adb6cea179128b33cb7e8bf
haokee-git <19173155158@163.com>
2026-01-31 02:17:49 +0800

    添加 ExperimentalFoundationApi 注解
    - animateItemPlacement 需要 @OptIn(ExperimentalFoundationApi::class)
    - 解决实验性 API 警告

--- app/src/main/java/org/haokee/recorder/ui/component/ThoughtList.kt
+++ app/src/main/java/org/haokee/recorder/ui/component/ThoughtList.kt
@@ -5,6 +5,7 @@ import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.animation.core.tween
 import androidx.compose.animation.fadeIn
 import androidx.compose.animation.fadeOut
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
@@ -25,6 +26,7 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import org.haokee.recorder.data.model.Thought
 
+@OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun ThoughtList(
     transcribedThoughts: List<Thought>,
